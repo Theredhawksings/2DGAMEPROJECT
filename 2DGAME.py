1afe304271187c6a61a8da559927246da41e4ccc
Theredhawksings <myeoseul@gmail.com>
2024-10-08 12:02:02 +0900

    위로가기 추가
    wwwwwwwwwwwww

--- 2DGAME.py
+++ 2DGAME.py
@@ -8,12 +8,18 @@ class Boy:
         self.x,self.y = random.randint(0,700),90
         self.frame = 0
         self.image = load_image('run_animation1.png')
+        self.dx, self.dy = 0, 0
 
     def update(self):
         self.frame = (self.frame+1)%3
+        self.x += self.dx
+        self.y += self.dy
 
     def draw(self):
         self.image.clip_draw(self.frame*64, 64, 64, 64, self.x, self.y, 128,128)
+
+    def move(self,dx,dy):
+        self.dx, self.dy = dx, dy
 
 def reset_world():
     global boy
@@ -37,6 +43,8 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_w:
+            boy.move(0, 5)
 
 open_canvas()
 
