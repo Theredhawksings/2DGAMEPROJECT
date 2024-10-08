5fec6255f2008d4757f0299d8f10a0fa90568a3d
Theredhawksings <myeoseul@gmail.com>
2024-10-08 11:56:52 +0900

    키보드 입력 생성

--- 2DGAME.py
+++ 2DGAME.py
@@ -17,7 +17,9 @@ class Boy:
 
 def reset_world():
     global boy
+    global running
     boy = Boy()
+    running = True
 
 def update_world():
     boy.update()
@@ -27,12 +29,21 @@ def render_world():
     boy.draw()
     update_canvas()
 
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
 
 open_canvas()
 
 reset_world()
 
-while True:
+while running:
+    handle_events()
     update_world()
     render_world()
     delay(0.05)
